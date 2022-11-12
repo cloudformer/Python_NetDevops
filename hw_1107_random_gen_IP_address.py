59f1826d78d8abe693e10788d241f9bccbe15ef0
cloudformer <114937121+cloudformer@users.noreply.github.com>
2022-11-12 21:10:09 +0800

    Create hw_1107_random_gen_IP_address.py

---
+++ hw_1107_random_gen_IP_address.py
@@ -0,0 +1,7 @@
+import random
+se1 = random.randint(1,255)
+se2 = random.randint(1,255)
+se3 = random.randint(1,255)
+se4 = random.randint(1,255)
+random_ip = str(se1) + '.' + str(se2) + '.' + str(se3) + '.' + str(se4)
+print(random_ip)
