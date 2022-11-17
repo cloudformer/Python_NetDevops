fefe39079fe3dc66d77041bb293e4515132802e8
cloudformer <114937121+cloudformer@users.noreply.github.com>
2022-11-18 00:11:45 +0800

    Update hw_1117_get_cisco_if_br.py

--- hw_1117_get_cisco_if_br.py
+++ hw_1117_get_cisco_if_br.py
@@ -22,18 +22,18 @@ def qytang_ssh(ip,username,password,port=22,cmd='ls'):
     return x
 from pprint import pprint
 def qytang_get_if(ips,username,password):
-    # print(f'ips={ips}')
-    # print(f'len={len(ips)}')
+    print(f'ips={ips}')
+    print(f'len={len(ips)}')
     iplist=[]
     # iplist.append(ips)
     iplist = ips
+    device_if_dict = {}
     print(f'iplist={iplist}')
     for ip in iplist:
         # print(ip)
         result = qytang_ping(ip)
         if result == '!':
             show_all_interface = qytang_ssh(ip, username, password, cmd='show ip int brief')
-            device_if_dict = {}
             interface = {}
             show_line_interface = re.split('\n', show_all_interface)
             res = []
@@ -50,5 +50,5 @@ def qytang_get_if(ips,username,password):
     return device_if_dict
 #
 if __name__=='__main__':
-    qytang_get_if(ips=('54.210.75.14','1.2.3.4','2.2.2.2'), username='cisco', password='Cisc0123')
+    qytang_get_if(ips=('172.20.10.110','172.20.10.10','172.20.10.210'), username='cisco', password='Cisc0123')
     # qytang_get_if(ips=['54.210.75.14'], username='cisco', password='Cisc0123')
